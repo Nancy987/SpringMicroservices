d880f98ce57c4e7042b734bc46a07bcae83d7f9c
tech200 <nancymangla199@gmail.com>
2022-06-23 01:00:36 +0530

    Initial Commit

---
+++ src/main/java/com/Prograd/MicroservicesDemo/MicroservicesDemoApplication.java
@@ -0,0 +1,15 @@
+package com.Prograd.MicroservicesDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MicroservicesDemoApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
